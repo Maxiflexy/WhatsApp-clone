71b2ec5762038c401e41be4b7def960f145e5f27
Maxiflexy <onyemax247@gmail.com>
2025-02-13 18:35:55 +0100

    created the chats service

---
+++ whatsappclone/src/main/java/com/maxiflexy/whatsappclone/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.maxiflexy.whatsappclone.repository;
+
+import com.maxiflexy.whatsappclone.user.User;
+import com.maxiflexy.whatsappclone.user.UserConstants;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query(name = UserConstants.FIND_USER_BY_EMAIL)
+    Optional<User> findByEmail(@Param("email") String userEmail);
+
+    @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
+    Optional<User> findByPublicId(String publicId);
+}
